2ad714e3cece14e3b0b6cda541371d5f369b8b33
Robert Chu <allegrormc@gmail.com>
2020-10-04 19:06:16 -0700

    Adds auth-type option.

--- web/src/pages/ProtocolEditorPage.tsx
+++ web/src/pages/ProtocolEditorPage.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Form } from 'react-bootstrap';
+import { useRecoilValueLoadable } from 'recoil';
+import { createEditor, Node } from 'slate';
+import { Slate, Editable, withReact } from 'slate-react';
+import { protocolsQuery } from '../state/selectors';
 
 export function ProtocolEditorPage() {
-    return <div>
-        TODO: ProtocolEditorPage
-    </div>
+    const editor = React.useMemo(() => withReact(createEditor()), []);
+    const [description, setDescription] = React.useState<Node[]>([]);
+
+    const protocols = useRecoilValueLoadable(protocolsQuery);
+
+    return <Form>
+        <Form.Group controlId="formProtocolTitle">
+            <Form.Label>Protocol Title</Form.Label>
+            <Form.Control type="text" />
+        </Form.Group>
+        <Slate editor={editor} value={description} onChange={newValue => setDescription(newValue)}>
+            <Editable />
+        </Slate>
+
+        {}
+    </Form>
 }
